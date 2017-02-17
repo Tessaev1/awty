33b70db2e5b69475c7c2fb6051dd015eba7eb3d2
Tessaev1 <tessaev1@uw.edu>
2017-02-17 14:23:43 -0800

    sets alarm according to user-given duration

---
+++ app/src/main/java/edu/uw/tessa/awty/AlarmReceiver.java
@@ -0,0 +1,31 @@
+package edu.uw.tessa.awty;
+
+import android.app.Activity;
+import android.content.*;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+/**
+ * Created by Tessa on 2/16/17.
+ */
+
+public final class AlarmReceiver extends BroadcastReceiver {
+    public final static String TAG = "AlarmReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String toast = MainActivity.phoneNumber + ": " + MainActivity.message;
+        Toast.makeText(context, toast, Toast.LENGTH_SHORT).show();
+
+        /*
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(phoneNumberReciver, null, message, null, null);
+        Toast.makeText(context, "Alarm Triggered and SMS Sent", Toast.LENGTH_LONG).show();
+         */
+    }
+}
--- app/src/main/java/edu/uw/tessa/awty/MainActivity.java
+++ app/src/main/java/edu/uw/tessa/awty/MainActivity.java
@@ -1,6 +1,9 @@
 package edu.uw.tessa.awty;
 
-import android.content.Context;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,33 +11,57 @@ import android.view.View;
 import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
-    Button start;
-    Toast messageSent;
-    String message;
-    String phoneNumber;
+    public EditText messageInput;
+    public EditText phoneInput;
+    public EditText durationInput;
+    public static String message;
+    public static String phoneNumber;
+    public int duration;
 
-    public static String TAG = "MainActivity";
+    public final static String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        Context context = getApplicationContext();
-//        int duration = Toast.LENGTH_SHORT;
+        Intent intent = new Intent(this, AlarmReceiver.class);
 
-        final EditText messageInput = (EditText) findViewById(R.id.message);
-        final EditText phoneInput = (EditText) findViewById(R.id.number);
+        final PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 1, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        final AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
 
-        start = (Button) findViewById(R.id.startButton);
+        final Button start = (Button) findViewById(R.id.startButton);
         start.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                message = messageInput.getText().toString();
-                Log.i(TAG, "message: " + message);
-                phoneNumber = phoneInput.getText().toString();
-                messageSent = Toast.makeText(getApplicationContext(), phoneNumber + ": " + message, Toast.LENGTH_SHORT);
-                messageSent.show();
+                if (start.getText().toString().toLowerCase().equals("start")) {
+                    start.setText("Stop");
+                    getInputFields();
+
+                    alarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                            SystemClock.elapsedRealtime(), duration * 60 * 1000, pendingIntent);
+                } else {
+                    start.setText("Start");
+                    if (alarmManager!= null) {
+                        alarmManager.cancel(pendingIntent);
+                    }
+                }
             }
         });
+    }
+
+    public void getInputFields() {
+        messageInput = (EditText) findViewById(R.id.message);
+        message = messageInput.getText().toString();
+
+        phoneInput = (EditText) findViewById(R.id.number);
+        phoneNumber = phoneInput.getText().toString();
+
+        durationInput = (EditText) findViewById(R.id.duration);
+        duration = Integer.parseInt(durationInput.getText().toString());
+        if (duration < 1) {
+            findViewById(R.id.durationError).setVisibility(View.VISIBLE);
+        } else {
+            findViewById(R.id.durationError).setVisibility(View.INVISIBLE);
+        }
     }
 }
